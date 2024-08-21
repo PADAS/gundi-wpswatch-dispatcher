ab535459a429618c1a5675c2761aa4353a04ddc0
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-08-21 09:32:40 -0300

    Add dispatchers for events and attachmetns in v2

--- app/services/dispatchers.py
+++ app/services/dispatchers.py
@@ -6,7 +6,7 @@ from app.core import settings
 from urllib.parse import urlparse
 from gundi_core import schemas
 from gcloud.aio.storage import Storage
-from app.core.utils import RateLimiterSemaphore, redis_client
+from app.core.utils import RateLimiterSemaphore, redis_client, find_config_for_action
 
 gcp_storage = Storage()
 
@@ -91,6 +91,110 @@ class WPSWatchCameraTrapDispatcher:
         )  # in case tailing forward slash configured in portal
         sanitized_endpoint = f"{scheme}://{host}{path}"
         return sanitized_endpoint
+
+
+class WPSWatchImageDispatcher:
+    def __init__(self, integration):
+        self.integration = integration
+
+    async def _wpswatch_post(self, request_data, file_name, file_data):
+        # Look for the configuration of the authentication action
+        configurations = self.integration.configurations
+        integration_action_config = find_config_for_action(
+            configurations=configurations,
+            action_value=schemas.v2.WPSWatchActions.AUTHENTICATE.value,
+        )
+        if not integration_action_config:
+            raise ValueError(
+                f"Authentication settings for integration {str(self.integration.id)} are missing. Please fix the integration setup in the portal."
+            )
+        api_key = integration_action_config.data.get("api_key")
+        if not api_key:
+            raise ValueError(
+                f"Token for integration {str(self.integration.id)} is missing. Please fix the integration setup in the portal."
+            )
+        headers = {
+            "Wps-Api-Key": api_key,
+        }
+        files = {"Attachment1": (file_name, file_data)}
+        parsed_url = urlparse(self.integration.base_url)
+        sanitized_endpoint = f"{parsed_url.scheme}://{parsed_url.hostname}/api/Upload"
+        try:
+            connect_timeout, read_timeout = settings.DEFAULT_REQUESTS_TIMEOUT
+            timeout_settings = httpx.Timeout(read_timeout, connect=connect_timeout)
+            async with httpx.AsyncClient(timeout=timeout_settings) as client:
+                response = await client.post(
+                    sanitized_endpoint,
+                    data=request_data,
+                    headers=headers,
+                    files=files,
+                )
+            response.raise_for_status()
+        except httpx.HTTPError as e:
+            logger.exception(
+                f"Error occurred posting to WPS Watch Site {sanitized_endpoint} \n {type(e)}: {e}"
+            )
+            raise e  # Raise so it's retried
+        return response
+
+    async def send(self, image: schemas.v2.WPSWatchImage, **kwargs):
+        related_event = kwargs.get("related_event")
+        if not related_event:
+            raise ValueError("related_observation is required")
+        camera_id = related_event.camera_id
+        if not camera_id:
+            raise ValueError("camera_id is required")
+
+        try:  # Download the Image from GCP
+            file_path = image.file_path
+            downloaded_file = await gcp_storage.download(
+                bucket=settings.BUCKET_NAME, object_name=file_path
+            )
+        except Exception as e:
+            logger.exception(
+                f"Error downloading file '{file_path}' from cloud storage: {type(e)}: {e}"
+            )
+            raise e
+
+        # Get the upload domain
+        configurations = self.integration.configurations
+        integration_push_config = find_config_for_action(
+            configurations=configurations,
+            action_value=schemas.v2.WPSWatchActions.PUSH_EVENTS.value,
+        )
+        if integration_push_config and (
+            upload_domain := integration_push_config.data.get("upload_domain")
+        ):
+            wpswatch_upload_domain = upload_domain
+        else:
+            wpswatch_upload_domain = "upload.wpswatch.org"  # Default
+
+        try:  # Send the image to WPS Watch
+            async with RateLimiterSemaphore(
+                redis_client=redis_client, url=str(self.integration.base_url)
+            ):
+                request_data = {
+                    "From": "gundiservice.org",
+                    "To": f"{camera_id}@{wpswatch_upload_domain}",
+                }
+                file_name = os.path.basename(file_path)
+                result = await self._wpswatch_post(
+                    request_data=request_data,
+                    file_name=file_name,
+                    file_data=downloaded_file,
+                )
+        except Exception as e:
+            logger.exception(f"Error sending data to WPS Watch {e}")
+            raise e
+        else:
+            logger.info(f"File {file_path} delivered to WPS Watch with success.")
+            # Remove the file from GCP after delivering it to WPS Watch
+            if settings.DELETE_FILES_AFTER_DELIVERY:
+                await gcp_storage.delete(
+                    bucket=settings.BUCKET_NAME, object_name=file_path
+                )
+                logger.debug(f"File {file_path} deleted from GCP.")
+            return result
 
 
 dispatcher_cls_by_type = {
